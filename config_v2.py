49e31f5fd61894f9f24529207698f44967b7f2d2
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-09-20 13:46:05 -0500

    Informe mensual agosto 2024
    Informe mensual agosto 2024

--- config_v2.py
+++ config_v2.py
@@ -4,11 +4,11 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically all
 # dates must be mondays.
-BASELINE = ['2024-04-01', '2024-07-01']
-STUDY = ['2024-07-01', '2024-08-01']
-PAST_MONTH= ['2024-06-01', '2024-07-01']
+BASELINE = ['2024-05-01', '2024-08-01']
+STUDY = ['2024-08-01', '2024-09-01']
+PAST_MONTH= ['2024-07-01', '2024-08-01']
 
-MONTH_NAME = 'Julio'
+MONTH_NAME = 'Agosto'
 
 DATE_INTERVALS_TO_DISCARD = {
 }
